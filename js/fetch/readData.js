c2299a4b31cebdd474c9e830fe084730ad6fca97
Rey <emmanuelle.rey@outlook.com>
2025-05-06 16:55:51 +0200

    update

---
+++ js/fetch/readData.js
@@ -0,0 +1,43 @@
+// *** Récupération des travaux présents dans le JSON ***/ 
+export async function loadDataRadars() {
+  return await getData("../../geojson/radars2018.geojson");
+} 
+
+export async function loadDataUsagers() {
+  return await getData("../../geojson/usagersGPS31.geojson");
+} 
+
+export async function loadDataVehicules() {
+  return await getData("../../geojson/vehiculesGPS31.geojson");
+} 
+
+export async function loadDataCommunes() {
+  return await getData("../../geojson/communes-haute-garonne-586-communes.geojson");
+} 
+
+export async function loadDataLieuxAccidents() {
+  return await getData("../../geojson/caracteristiquesLieuxGPS31.geojson");
+} 
+
+export async function loadDataLieuxVehiculesAccidents() {
+  return await getData("../../geojson/caracteristiques-lieux-vehiculesVF.geojson");
+} 
+
+
+
+// *** Le seul et unique Fetch ***//
+async function getData(path) {
+  try {
+    const response = await fetch(path);
+    if (!response.ok) {
+      throw new Error("Impossible de charger les données");
+    }
+    console.log("Module readData.js chargé !");
+    return await response.json();
+  } 
+  catch (error) {
+    console.error("Erreur lors du fetch :", error.message);
+    return []; 
+  }
+}
+
