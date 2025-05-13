5ca31363eba2c8d61ae1605d4af077a25597afa9
Rey <emmanuelle.rey@outlook.com>
2025-05-13 16:47:55 +0200

    Corrections

--- js/index.js
+++ js/index.js
@@ -3,14 +3,9 @@ import * as readData from "./fetch/readData.js"
 import * as modale from "./modale/modale.js"
 import * as introduction from "./ui/introduction.js"
 import * as map from "./map/index.js"
-import * as treemapDonnesAccidents from "./chart/treemapMotifDeplacement.js"
-import * as barChartGraviteAccidents from "./chart/barchartGraviteAccidents.js"
-import * as pieChartEclairage from "./chart/piechartEclairage.js"
-import * as pieChartConditionsAtmos from "./chart/piechartConditionsAtmosph.js"
 import * as dataAndCharts from "./ui/index.js"
 
 // Variables globales : tableaux vides
-export let RADARS = []
 export let USAGERS = []
 export let VEHICULES = []
 export let COMMUNES = []
@@ -19,19 +14,17 @@ export let LIEUX_VEHICULES_ACCIDENTS = []
 
 // Récupère les données du backend
 async function getAllData() {
-  RADARS = await readData.loadDataRadars()
   USAGERS = await readData.loadDataUsagers()
   VEHICULES = await readData.loadDataVehicules()
   COMMUNES = await readData.loadDataCommunes()
   LIEUX_ACCIDENTS = await readData.loadDataLieuxAccidents()
   LIEUX_VEHICULES_ACCIDENTS = await readData.loadDataLieuxVehiculesAccidents()
   
-  console.log("Données radars", RADARS)
-  console.log("Données usagers", USAGERS)
-  console.log("Données véhicules", VEHICULES)
-  console.log("Données communes", COMMUNES)
-  console.log("Données lieux accidents", LIEUX_ACCIDENTS)
-  console.log("Données lieux / véhicules accidents", LIEUX_VEHICULES_ACCIDENTS)
+  //console.log("Données usagers", USAGERS)
+  //console.log("Données véhicules", VEHICULES)
+  //console.log("Données communes", COMMUNES)
+  //console.log("Données lieux accidents", LIEUX_ACCIDENTS)
+  //console.log("Données lieux / véhicules accidents", LIEUX_VEHICULES_ACCIDENTS)
 }
 
 // Point d'entrée
@@ -40,12 +33,8 @@ async function main(){
     await getAllData()
     modale.displayModale()
     introduction.blocIntroduction()
-    dataAndCharts.displayMainData()
     map.initMap()
-    treemapDonnesAccidents.displayDataAccidentsTreemap()    
-    barChartGraviteAccidents.graviteAccidents()
-    pieChartEclairage.conditionsEclairage()
-    pieChartConditionsAtmos.conditionsAtmospheriques()
+    dataAndCharts.displayCharts()
   } catch(e) {
     console.log(e)
   }
