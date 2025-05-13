3264247a1440d9dd68b8be1e63cce3365bba30bd
Rey <emmanuelle.rey@outlook.com>
2025-05-13 16:47:24 +0200

    Corrections

--- js/ui/index.js
+++ js/ui/index.js
@@ -1,119 +1,28 @@
 import * as totaux from "../total/index.js"
+import * as allCharts from "./displayAllCharts.js"
 import * as treemapVehiculesImpliques from "../chart/treemapVehiculesLesPlusImpliques.js"
+import * as treemapMotifDeplacement from "../chart/treemapMotifDeplacement.js"
+import * as barChartGraviteAccidents from "../chart/barchartGraviteAccidents.js"
+import * as pieChartEclairage from "../chart/piechartEclairage.js"
+import * as pieChartAsmospherique from "../chart/piechartConditionsAtmosph.js"
 
-export function displayMainData (){
-    createMainData()
+export function displayCharts(){
+    allCharts.displayMainData()
     totaux.calculTotalNombreAccidents()
     totaux.calculTotalNombreVehiculesImpliques()
     totaux.calculPersonnesDecedees()
     totaux.calculTotalNombrePersonnesImpliquees()
     
-    mainVehiclesInvolved()
+    allCharts.displayTreemapTypeOfVehicle()
     treemapVehiculesImpliques.displayCategoryVehicules()
+   
+    allCharts.displayBarChart()
+    barChartGraviteAccidents.graviteAccidents()
     
+    allCharts.displayPieCharts()
+    pieChartEclairage.conditionsEclairage()
+    pieChartAsmospherique.conditionsAtmospheriques()
+
+    allCharts.displayTreeMapMotifDeplacement()
+    treemapMotifDeplacement.displayDataAccidentsTreemap()
 }
-
-function createMainData(){
-    let mainDataContainer = document.getElementById("mainDataContainer")
-        mainDataContainer.classList.add("mainDataContainer")
-    
-    let mainDataSubcontainer = document.createElement("div")
-        mainDataSubcontainer.classList.add("mainDataSubcontainer")
-    let mainDataTitle = document.createElement("h2")
-        mainDataTitle.innerHTML="Principales données sur les accidents corporels en 2022"
-    
-    let dataContainer0 = document.createElement("div") 
-        dataContainer0.classList.add("dataContainer0")
-    let dataContainer1 = document.createElement("div") 
-        dataContainer1.classList.add("donneesTitreetChiffre")
-    let dataAndTitleContainer1 = document.createElement("div") 
-        dataAndTitleContainer1.classList.add("dataAndTitle")
-    let dataTitle1 = document.createElement("div") 
-        dataTitle1.classList.add("donneesTitre")
-        dataTitle1.innerHTML="Nombre total d'accidents"
-    let dataNumber1 = document.createElement("div") 
-        dataNumber1.classList.add("donneesChiffre")
-        dataNumber1.id="elementIdTotalAccident"
-
-    let dataContainer2 = document.createElement("div") 
-        dataContainer2.classList.add("donneesTitreetChiffre")
-    let dataAndTitleContainer2 = document.createElement("div") 
-        dataAndTitleContainer2.classList.add("dataAndTitle")
-    let dataTitle2 = document.createElement("div") 
-        dataTitle2.classList.add("donneesTitre")
-        dataTitle2.innerHTML="Nombre total de véhicules impliqués"
-    let dataNumber2 = document.createElement("div") 
-        dataNumber2.classList.add("donneesChiffre")
-        dataNumber2.id="elementIdVehiculesImpliques"
-    
-    let dataContainer3 = document.createElement("div") 
-        dataContainer3.classList.add("donneesTitreetChiffre")
-    let dataAndTitleContainer3 = document.createElement("div") 
-        dataAndTitleContainer3.classList.add("dataAndTitle")
-    let dataTitle3 = document.createElement("div") 
-        dataTitle3.classList.add("donneesTitre")
-        dataTitle3.innerHTML="Nombre de personnes impliquées"
-    let dataNumber3 = document.createElement("div") 
-        dataNumber3.classList.add("donneesChiffre")
-        dataNumber3.id="elementIdPersonnesImpliquees"
-
-    let dataContainer4 = document.createElement("div") 
-        dataContainer4.classList.add("donneesTitreetChiffre")
-    let dataAndTitleContainer4 = document.createElement("div") 
-        dataAndTitleContainer4.classList.add("dataAndTitle")
-    let dataTitle4 = document.createElement("div") 
-        dataTitle4.classList.add("donneesTitre")
-        dataTitle4.innerHTML="Nombre de personnes décédées"
-    let dataNumber4 = document.createElement("div") 
-        dataNumber4.classList.add("donneesChiffre")
-        dataNumber4.id="elementIdDeces"
-
-
-    mainDataContainer.appendChild(mainDataSubcontainer)
-    mainDataSubcontainer.appendChild(mainDataTitle)
-    mainDataSubcontainer.appendChild(dataContainer0)
-    dataContainer0.appendChild(dataContainer1)
-    dataContainer1.appendChild(dataAndTitleContainer1)
-    dataAndTitleContainer1.appendChild(dataTitle1)
-    dataAndTitleContainer1.appendChild(dataNumber1)
-
-    mainDataSubcontainer.appendChild(dataContainer0)
-    dataContainer0.appendChild(dataContainer2)
-    dataContainer2.appendChild(dataAndTitleContainer2)
-    dataAndTitleContainer2.appendChild(dataTitle2)
-    dataAndTitleContainer2.appendChild(dataNumber2)
-
-    mainDataSubcontainer.appendChild(dataContainer0)
-    dataContainer0.appendChild(dataContainer3)
-    dataContainer3.appendChild(dataAndTitleContainer3)
-    dataAndTitleContainer3.appendChild(dataTitle3)
-    dataAndTitleContainer3.appendChild(dataNumber3)
-    
-    mainDataSubcontainer.appendChild(dataContainer0)
-    dataContainer0.appendChild(dataContainer4)
-    dataContainer4.appendChild(dataAndTitleContainer4)
-    dataAndTitleContainer4.appendChild(dataTitle4)
-    dataAndTitleContainer4.appendChild(dataNumber4)
-    
-}
-
-function mainVehiclesInvolved(){
-    let mainDataContainer = document.getElementById("mainDataContainer")
-    let mainDataSubcontainer = document.createElement("div")
-        mainDataSubcontainer.classList.add("mainDataSubcontainer")
-    let mainDataTitle = document.createElement("h2")
-        mainDataTitle.innerHTML="Types de véhicules les plus impliqués dans un accident corporel"
-    let lectureGraphique = document.createElement("p")
-        lectureGraphique.innerHTML = "Lecture du graphique : en 2022 en Haute-Garonne, parmi les 1028 véhicules impliqués dans des accidents corporels, 63% concernaient des voitures."
-    let chart = document.createElement("div")
-        chart.id="chartParCategorieDeVehicule"
-        
-    mainDataContainer.appendChild(mainDataSubcontainer)
-    mainDataSubcontainer.appendChild(mainDataTitle)
-    mainDataSubcontainer.appendChild(lectureGraphique)
-    mainDataSubcontainer.appendChild(chart)
-}
-
-
-
-                
