f64f4a5a7acf642c8fd7176e98721f762cbbf938
Rey <emmanuelle.rey@outlook.com>
2025-05-13 17:42:18 +0200

    Test avec l'url pour Data Usagers

--- js/fetch/readData.js
+++ js/fetch/readData.js
@@ -1,7 +1,7 @@
 // *** Récupération des travaux présents dans le JSON ***/ 
 
 export async function loadDataUsagers() {
-  return await getData("https://raw.githubusercontent.com/Emy2024/projet1-accidents-corporels-31/refs/heads/main/js/fetch/readData.js");
+  return await getData("https://raw.githubusercontent.com/Emy2024/projet1-accidents-corporels-31/refs/heads/main/geojson/usagersGPS31.geojson");
 } 
 
 export async function loadDataVehicules() {
