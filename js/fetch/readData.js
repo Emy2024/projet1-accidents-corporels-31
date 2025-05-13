56c2e82e7f7b8c5aa08b90269d8b4f33a274519a
Rey <emmanuelle.rey@outlook.com>
2025-05-13 21:35:54 +0200

    Nouveaux chemins URL

--- js/fetch/readData.js
+++ js/fetch/readData.js
@@ -1,23 +1,23 @@
 // *** Récupération des travaux présents dans le JSON ***/ 
 
 export async function loadDataUsagers() {
-  return await getData("../../geojson/usagersGPS31.geojson");
+  return await getData("https://raw.githubusercontent.com/Emy2024/projet1-accidents-corporels-31/refs/heads/main/geojson/usagersGPS31.geojson");
 } 
 
 export async function loadDataVehicules() {
-  return await getData("../../geojson/vehiculesGPS31.geojson");
+  return await getData("https://raw.githubusercontent.com/Emy2024/projet1-accidents-corporels-31/refs/heads/main/geojson/vehiculesGPS31.geojson");
 } 
 
 export async function loadDataCommunes() {
-  return await getData("../../geojson/communes-haute-garonne-586-communes.geojson");
+  return await getData("https://raw.githubusercontent.com/Emy2024/projet1-accidents-corporels-31/refs/heads/main/geojson/communes-haute-garonne-586-communes.geojson");
 } 
 
 export async function loadDataLieuxAccidents() {
-  return await getData("../../geojson/caracteristiquesLieuxGPS31.geojson");
+  return await getData("https://raw.githubusercontent.com/Emy2024/projet1-accidents-corporels-31/refs/heads/main/geojson/caracteristiquesLieuxGPS31.geojson");
 } 
 
 export async function loadDataLieuxVehiculesAccidents() {
-  return await getData("../../geojson/caracteristiques-lieux-vehiculesVF.geojson");
+  return await getData("https://raw.githubusercontent.com/Emy2024/projet1-accidents-corporels-31/refs/heads/main/geojson/vehiculesGPS31.geojson");
 } 
 
 
