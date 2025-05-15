1ad6d3d7b62bc9a1611d21f3933f326b4f27c7d4
Rey <emmanuelle.rey@outlook.com>
2025-05-15 16:38:10 +0200

    Update

--- js/ui/introduction.js
+++ js/ui/introduction.js
@@ -6,6 +6,11 @@ export function blocIntroduction() {
     h1.classList.add("introductionH1");
     h1.textContent = "Panorama des accidents corporels en Haute-Garonne en 2022";
 
+    const btn__returnPortfolio = document.getElementById("btn__returnPortfolio")
+    btn__returnPortfolio.textContent = "Consulter mon portfolio"
+    btn__returnPortfolio.classList.add("btn__returnPortfolio")
+    btn__returnPortfolio.href="https://emy2024.github.io/portfolio/"
+
     const paragraph = document.createElement("p");
     paragraph.classList.add("sourceINSEE");
     paragraph.textContent = "Source : Bulletins d’Analyse des Accidents Corporels de la Circulation (BAAC) ";
@@ -20,7 +25,6 @@ export function blocIntroduction() {
 
     const quoteText = document.createElement("span");
     quoteText.textContent = "« Pour chaque accident corporel (soit un accident survenu sur une voie ouverte à la circulation publique, impliquant au moins un véhicule et ayant fait au moins une victime ayant nécessité des soins), des saisies d’information décrivant l’accident sont effectuées par l’unité des forces de l’ordre (police, gendarmerie, etc.) qui est intervenue sur le lieu de l’accident » - Extrait de la description issue de la documentation du fichier BACC »";
-
 
     blockquote.appendChild(quoteText);
 
